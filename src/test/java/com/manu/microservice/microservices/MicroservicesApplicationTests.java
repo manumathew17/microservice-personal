9c1760fb7208fe2cc002e6d70bbd3ae267cc7787
manu mathew <manu@easyecom.io>
2022-06-26 20:39:55 +0530

    init commit

---
+++ src/test/java/com/manu/microservice/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manu.microservice.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
